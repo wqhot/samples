1f1690cdc62e9faedcd01e3e083d3a2021d3d33c
fuyangchenghu <1015138540@qq.com>
2020-11-09 22:27:44 -0800

    change 200dk_peripheral

---
+++ level1_single_api/5_200dk_peripheral/gpio/build/cmake/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ascend/samples/level1_single_api/5_200dk_peripheral/gpio/gpio/src/gpio.cpp" "/home/ascend/samples/level1_single_api/5_200dk_peripheral/gpio/gpio/build/cmake/src/CMakeFiles/main.dir/gpio.cpp.o"
+  "/home/ascend/samples/level1_single_api/5_200dk_peripheral/gpio/gpio/src/main.cpp" "/home/ascend/samples/level1_single_api/5_200dk_peripheral/gpio/gpio/build/cmake/src/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ascend/Ascend/ascend-toolkit/20.0.RC1/acllib_centos7.6.aarch64/acllib/include"
+  "../../src/../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ level1_single_api/5_200dk_peripheral/gpio/build/cmake/src/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../../out/main"
+  "../../../out/main.pdb"
+  "CMakeFiles/main.dir/gpio.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
