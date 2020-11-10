af97498fba2cc074637299639c5f754aa60b6135
fuyangchenghu <1015138540@qq.com>
2020-11-09 19:44:10 -0800

    commit venc

--- level1_single_api/1_acl/4_dvpp/venc/src/main.h
+++ level1_single_api/1_acl/4_dvpp/venc/src/main.h
@@ -1,5 +1,4 @@
 /**
-* @file utils.h
 *
 * Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
 *
@@ -21,18 +20,27 @@
 #include "acl/ops/acl_dvpp.h"
 #include <cstdint>
 
-//#define INFO_LOG(fmt, args...) fprintf(stdout, "[INFO]  " fmt "\n", ##args)
-//#define WARN_LOG(fmt, args...) fprintf(stdout, "[WARN]  " fmt "\n", ##args)
-//#define ERROR_LOG(fmt, args...) fprintf(stdout, "[ERROR] " fmt "\n", ##args)
+#define INFO_LOG(fmt, args...) fprintf(stdout, "[INFO]  " fmt "\n", ##args)
+#define WARN_LOG(fmt, args...) fprintf(stdout, "[WARN]  " fmt "\n", ##args)
+#define ERROR_LOG(fmt, args...) fprintf(stdout, "[ERROR]  " fmt "\n", ##args)
 
-//typedef enum Result {
-//    SUCCESS = 0,
-//    FAILED = 1
-//} Result;
+#define RGBU8_IMAGE_SIZE(width, height) ((width) * (height) * 3)
+#define YUV420SP_SIZE(width, height) ((width) * (height) * 3 / 2)
+
+#define ALIGN_UP(num, align) (((num) + (align) - 1) & ~((align) - 1))
+#define ALIGN_UP2(num) ALIGN_UP(num, 2)
+#define ALIGN_UP16(num) ALIGN_UP(num, 16)
+#define ALIGN_UP128(num) ALIGN_UP(num, 128)
 
 typedef struct PicDesc {
     std::string picName;
     int width;
     int height;
 }PicDesc;
+
+typedef enum Result {
+    SUCCESS = 0,
+    FAILED = 1
+}Result;
+
 
